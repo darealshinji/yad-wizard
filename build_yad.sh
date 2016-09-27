998f371b61d5d82dc732fd8a2289f7d1ff7515f0
darealshinji <djcj@gmx.de>
2016-09-27 02:22:14 +0200

    YAD v0.37.0

--- build_yad.sh
+++ build_yad.sh
@@ -6,7 +6,7 @@ else
   target="x86"
 fi
 
-ver="0.36.3"
+ver="0.37.0"
 dir="yad-$ver"
 txz="${dir}.tar.xz"
 rm -rf $dir
@@ -14,12 +14,14 @@ test -f $txz || wget "https://downloads.sourceforge.net/project/yad-dialog/$txz"
 tar xf $txz
 
 cd $dir
-patch -p1 < ../yad-patch-1.diff
-patch -p1 < ../yad-patch-2.diff
+patch -p1 < ../yad-patch-1-autotools.diff
+patch -p1 < ../yad-patch-1-main.diff
+patch -p1 < ../yad-patch-1-option.diff
+patch -p1 < ../yad-patch-2-misc.diff
 autoreconf -if
 XCFLAGS="-Os -Wformat -Werror=format-security -fno-stack-protector -fno-strict-aliasing -ffunction-sections -fdata-sections -D_FORTIFY_SOURCE=2"
 XLDFLAGS="-Wl,-z,defs -Wl,-z,norelro -Wl,--gc-sections -Wl,--as-needed"
-./configure --prefix=/usr --with-gtk=gtk2 --disable-gio
+./configure --prefix=/usr --with-gtk=gtk2 --disable-gio --disable-icon-browser
 
 make clean
 make -j4 V=1 XCFLAGS="$XCFLAGS" XLDFLAGS="$XLDFLAGS"
